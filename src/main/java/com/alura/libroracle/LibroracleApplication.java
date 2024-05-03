bebe635f27e93f1629ece46e6c25d454ccb769a0
Tomás Manoukian <tomanoukian@gmail.com>
2024-05-03 01:15:42 -0300

    Initial commit

---
+++ src/main/java/com/alura/libroracle/LibroracleApplication.java
@@ -0,0 +1,13 @@
+package com.alura.libroracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibroracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibroracleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alura/libroracle/LibroracleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.libroracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibroracleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
